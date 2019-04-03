cdfcb6f95eb7fcba3580c7f641e4c8d3a3ec714d
ericchan378 <ericchan378@gmail.com>
2019-04-02 21:50:14 -0400

    Update RodCutting.java

--- RodCutting.java
+++ RodCutting.java
@@ -1,3 +1,4 @@
+
 /**
  * Rod cutting problem described in Chapter 15 of textbook
  Author: Eric Chan
@@ -6,12 +7,58 @@ public class RodCutting {
 
   // Do not change the parameters!
   public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+	  //array of max revenue for a certain length rod for lookup
+	  int[] maxRev = new int[rodLength + 1];
+	  for(int i = 0; i < maxRev.length; i++)
+	  {
+		  //initialize to -1 indicating not yet stored
+		  maxRev[i] = -1;
+	  }
+	  return rodCuttingAux(rodLength, lengthPrices, maxRev);
+  }
+  
+  private int rodCuttingAux(int rodLength, int[] lengthPrices, int[] maxRev) {
+	  //greater than or equal to 0 would mean a maximum revenue was stored
+	  if (maxRev[rodLength] >= 0)
+	  {
+		  return maxRev[rodLength];
+	  }
+	  //for comparison not expecting anything else negative
+	  int max = -1;
+	  //no rod, no money
+	  if(rodLength == 0)
+	  {
+		  max = 0;
+	  }
+	  //recursive step to find max of that length
+	  else
+	  {
+		for(int i = 0; i < rodLength; i++)
+		{
+			max = Math.max(max, lengthPrices[i] + rodCuttingAux(rodLength - i - 1, lengthPrices, maxRev));
+		}
+	  }
+	  //memo
+	  maxRev[rodLength] = max;
+	  return max;
   }
 
   // Do not change the parameters!
   public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+	  //array of best/max values for some length rod
+	  int maxTable[] = new int[rodLength + 1];
+	  //rod length 0
+	  maxTable[0] = 0;
+	  //fill rest of array
+	  for (int i = 1; i <= rodLength; i++)
+	  {
+		  int max = -1;
+		  for (int j = 1; j <= i; j++)
+			  max = Math.max(max, lengthPrices[j - 1] + maxTable[i-j]);
+		  maxTable[i] = max;
+	  }
+	  
+    return maxTable[rodLength];
   }
 
 
